64b79680023e3bf83c82510c3431563586d38aff
Wendeley <wendeleyf@gmail.com>
2022-03-29 10:30:33 -0300

    Add model de Pessoa e Endereco

---
+++ src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
@@ -0,0 +1,70 @@
+package com.algaworks.algamoney.api.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
+
+@Entity
+@Table(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long codigo;
+
+    @NotNull
+    @Size (min = 1, max = 50)
+    private String nome;
+
+    @NotNull
+    private boolean ativo;
+
+    @Embedded
+    private Endereco endereco;
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return codigo == pessoa.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
