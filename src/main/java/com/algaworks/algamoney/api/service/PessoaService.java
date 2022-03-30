3c8c8b55be4b2b1f62c1cd67ae494bec3c8bcd6a
Wendeley <wendeleyf@gmail.com>
2022-03-30 17:07:59 -0300

    feat: add PutMapping para atualizar propriedade ativo de Pessoa

--- src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
+++ src/main/java/com/algaworks/algamoney/api/service/PessoaService.java
@@ -14,11 +14,22 @@ public class PessoaService {
     private PessoaRepository pessoaRepository;
 
     public Pessoa atualizar(Long codigo, Pessoa pessoa){
+        Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); //TODO explicação
+        return pessoaRepository.save(pessoaSalva); // Salvar pessoa no DB
+    }
+
+    public void atualizarPropriedadeAtivo(Long codigo, Boolean ativo){
+    Pessoa pessoaSalva = buscarPessoaPeloCodigo(codigo);
+    pessoaSalva.setAtivo(ativo);
+    pessoaRepository.save(pessoaSalva);
+    }
+
+    private Pessoa buscarPessoaPeloCodigo(Long codigo) {
         Pessoa pessoaSalva = pessoaRepository.findById(codigo).orElse(null); // Buscar pessoa pelo ID no DB
         if(pessoaSalva == null){
             throw new EmptyResultDataAccessException(1);
         }
-        BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo"); //TODO explicação
-        return pessoaRepository.save(pessoaSalva); // Salvar pessoa no DB
+        return pessoaSalva;
     }
 }
